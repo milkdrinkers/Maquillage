a6bbd2bcd458d6542e4f25d53c93de840cf18a83
darksaid98 <darksaid9889@gmail.com>
2023-12-25 21:12:29 +0100

    fix(V1__create_tables): foreign keys & constraints

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -16,12 +16,16 @@ CREATE TABLE IF NOT EXISTS "${tablePrefix}colors" (
 CREATE TABLE IF NOT EXISTS "${tablePrefix}tags_players" (
     "player" ${uuidType} NOT NULL,
     "tag" int NOT NULL,
-    PRIMARY KEY ("player")
+    PRIMARY KEY ("player"),
+    INDEX "tagid" ("tag"),
+    CONSTRAINT "tagid" FOREIGN KEY ("tag") REFERENCES ${tablePrefix}tags (id) ON DELETE CASCADE
 )${tableDefaults};
 
 
 CREATE TABLE IF NOT EXISTS "${tablePrefix}colors_players" (
     "player" ${uuidType} NOT NULL,
     "color" int NOT NULL,
-    PRIMARY KEY ("player")
+    PRIMARY KEY ("player"),
+    INDEX "colorid" ("color"),
+    CONSTRAINT "colorid" FOREIGN KEY ("color") REFERENCES ${tablePrefix}colors (id) ON DELETE CASCADE
 )${tableDefaults};
