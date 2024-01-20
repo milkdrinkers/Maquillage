c05701be2c2e330ab3ef12b2ca56665532ae307c
rooooose-b <142353909+rooooose-b@users.noreply.github.com>
2024-01-20 16:10:24 +0100

    fix: added space between tag and name in tag item lore

--- src/main/java/io/github/Alathra/Maquillage/gui/PopulateContent.java
+++ src/main/java/io/github/Alathra/Maquillage/gui/PopulateContent.java
@@ -100,7 +100,7 @@ public class PopulateContent {
         tagItemMeta.displayName(ColorParser.of((tag.getName())).build());
         List<Component> loreList = new ArrayList<>();
         // Add a component to the list that contains the player's name with the tag
-        loreList.add(ColorParser.of(tag.getTag() + "<white>" + p.getName()).build());
+        loreList.add(ColorParser.of(tag.getTag() + "<white> " + p.getName()).build());
         // Add a component to the list that describes action if clicked
         loreList.add(ColorParser.of("<grey>Click this to select the tag.").build());
         tagItemMeta.lore(loreList);
@@ -113,7 +113,7 @@ public class PopulateContent {
 
     public static void addSelectedTagItem(PaginatedGui gui, Tag tag, Player p) {
         selectedTagItemMeta.displayName(ColorParser.of("<white>" + tag.getName()).build());
-        selectedTagItemMeta.lore(Collections.singletonList(ColorParser.of(tag.getTag() + "<white>" + p.getName()).build()));
+        selectedTagItemMeta.lore(Collections.singletonList(ColorParser.of(tag.getTag() + "<white> " + p.getName()).build()));
         selectedTagItem.setItemMeta(selectedTagItemMeta);
         gui.addItem(ItemBuilder.from(selectedTagItem).asGuiItem(event -> gui.update()));
     }
