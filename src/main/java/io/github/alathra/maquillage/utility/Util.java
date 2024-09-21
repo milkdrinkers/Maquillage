fb3e09c31a9c2cf652918f25ac2bfe0cd565e697
rooooose-b <142353909+rooooose-b@users.noreply.github.com>
2024-09-22 01:39:21 +0200

    fix: clear minimessage from keys

--- src/main/java/io/github/alathra/maquillage/utility/Util.java
+++ src/main/java/io/github/alathra/maquillage/utility/Util.java
@@ -5,7 +5,17 @@ import java.util.HashMap;
 public class Util {
 
     public static String createKey(String label, HashMap<String, Integer> map) {
-        String keyNoNum = label.replaceAll("[^a-zA-Z ]", "").toLowerCase().replace(" ", "_");
+        String keyNoNum = label;
+
+        while (keyNoNum.contains("<")) {
+            int start = keyNoNum.indexOf("<");
+            int end = keyNoNum.indexOf(">");
+
+            String s = keyNoNum;
+            keyNoNum = s.substring(0, start) + s.substring(end);
+        }
+
+        keyNoNum = keyNoNum.replaceAll("[^a-zA-Z ]", "").toLowerCase().replace(" ", "_");
         String key = keyNoNum;
 
         int i = 1;
