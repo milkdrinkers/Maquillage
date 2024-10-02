450c6f7ff3ed7761cc7ce8265f4b1bdcafeffdc1
darksaid98 <darksaid9889@gmail.com>
2024-10-02 22:14:16 +0200

    refactor: zoink files

--- src/main/java/io/github/Alathra/Maquillage/module/cosmetic/CosmeticIdentifiable.java
+++
@@ -1,7 +0,0 @@
-package io.github.alathra.maquillage.module.cosmetic;
-
-public interface CosmeticIdentifiable {
-    String getKey();
-
-    void setKey(String key);
-}
--- src/main/java/io/github/Alathra/maquillage/translation/InternalTranslation.java
+++
@@ -1,6 +0,0 @@
-package io.github.alathra.maquillage.translation;
-
-import com.github.milkdrinkers.crate.Config;
-
-record InternalTranslation(String translationName, Config translation) {
-}
