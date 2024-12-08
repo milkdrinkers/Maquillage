21cb9fc14274c98ad5b08a71259f6bcaace1f04e
darksaid98 <darksaid9889@gmail.com>
2024-12-08 10:34:53 +0100

    test: add basic testing for tags and namecolors

--- src/test/java/io/github/alathra/maquillage/database/AbstractDatabaseTest.java
+++ src/test/java/io/github/alathra/maquillage/database/AbstractDatabaseTest.java
@@ -2,10 +2,14 @@ package io.github.alathra.maquillage.database;
 
 import io.github.alathra.maquillage.database.config.DatabaseConfig;
 import io.github.alathra.maquillage.database.exception.DatabaseInitializationException;
+import io.github.alathra.maquillage.database.sync.SyncHandler;
 import io.github.alathra.maquillage.utility.DB;
 import org.junit.jupiter.api.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static io.github.alathra.maquillage.database.schema.Tables.COLORS;
+import static io.github.alathra.maquillage.database.schema.tables.Tags.TAGS;
 
 /**
  * Contains all test cases.
@@ -50,24 +54,62 @@ abstract class AbstractDatabaseTest {
         DB.getHandler().migrate();
     }
 
-//    @Test
-//    @DisplayName("Upsert")
-//    void testUpsert() {
-//        Queries.upsert();
-//        Queries.upsert(); // Updates instead of inserts
-//    }
-//
-//    @Test
-//    @DisplayName("Upsert Returning")
-//    void testUpsertReturning() {
-//        BigInteger value = Queries.upsertReturning();
-//        Assertions.assertNotNull(value);
-//        Assertions.assertEquals(BigInteger.valueOf(1), value);
-//        BigInteger value2 = Queries.upsertReturning();
-//        Assertions.assertNotNull(value2);
-//        Assertions.assertEquals(BigInteger.valueOf(2), value2);
-//    }
-//
+    @Test
+    @DisplayName("Create Tags")
+    void testInsertReturningTag() {
+        for (int i = 1; i < 6; i++) {
+            final int databaseId = Queries.Tag.saveTag("<red>[Test]", "maquillage.tag.test", "Test");
+            Assertions.assertNotEquals(-1, databaseId);
+            Assertions.assertEquals(i, databaseId);
+
+            Queries.Sync.saveSyncMessage(SyncHandler.SyncAction.FETCH, SyncHandler.SyncType.TAG, databaseId);
+        }
+    }
+
+    @Test
+    @DisplayName("Create Namecolors")
+    void testInsertReturningNamecolor() {
+        for (int i = 1; i < 6; i++) {
+            final int databaseId = Queries.NameColor.saveColor("<red>", "maquillage.tag.test", "Test");
+            Assertions.assertNotEquals(-1, databaseId);
+            Assertions.assertEquals(i, databaseId);
+
+            Queries.Sync.saveSyncMessage(SyncHandler.SyncAction.FETCH, SyncHandler.SyncType.COLOR, databaseId);
+        }
+    }
+
+    @Test
+    @DisplayName("Select All Tags")
+    void testFetchAllTags() {
+        var result = Queries.Tag.loadAllTags();
+        Assertions.assertNotNull(result, "The fetched jOOQ Results are null!");
+
+        int startingInt = 0;
+        for (var _record : result) {
+            startingInt++;
+            Assertions.assertEquals(startingInt, _record.get(TAGS.ID));
+            Assertions.assertEquals("<red>[Test]", _record.get(TAGS.TAG));
+            Assertions.assertEquals("maquillage.tag.test", _record.get(TAGS.PERM));
+            Assertions.assertEquals("Test", _record.get(TAGS.LABEL));
+        }
+    }
+
+    @Test
+    @DisplayName("Select All Namecolors")
+    void testFetchAllNamecolors() {
+        var result = Queries.NameColor.loadAllColors();
+        Assertions.assertNotNull(result, "The fetched jOOQ Results are null!");
+
+        int startingInt = 0;
+        for (var _record : result) {
+            startingInt++;
+            Assertions.assertEquals(startingInt, _record.get(COLORS.ID));
+            Assertions.assertEquals("<red>", _record.get(COLORS.COLOR));
+            Assertions.assertEquals("maquillage.tag.test", _record.get(COLORS.PERM));
+            Assertions.assertEquals("Test", _record.get(COLORS.LABEL));
+        }
+    }
+
 //    @Test
 //    @DisplayName("Batch")
 //    void testQueryBatch() {
