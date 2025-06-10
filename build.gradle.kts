2b8a3b9f397d3b4c9aaa49221c37bffb47be4be5
darksaid98 <darksaid9889@gmail.com>
2025-06-10 19:05:38 +0200

    feat: publish api to maven central
    - Uploads release builds as maven releases
    - Uploads snapshot builds as maven snapshots

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,14 @@ import org.gradle.kotlin.dsl.jooqCodegen
 import org.gradle.kotlin.dsl.libs
 import org.jooq.meta.jaxb.Logging
 import java.time.Instant
+import com.vanniktech.maven.publish.JavaLibrary
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.SonatypeHost
 
 plugins {
     `java-library`
 
+    alias(libs.plugins.publisher)
     alias(libs.plugins.shadow) // Shades and relocates dependencies, see https://gradleup.com/shadow/
     alias(libs.plugins.run.paper) // Built in test server using runServer and runMojangMappedServer tasks
     alias(libs.plugins.plugin.yml) // Automatic plugin.yml generation
@@ -256,4 +260,66 @@ fun applyCustomVersion() {
 
     // Strip prefixed "v" from version tag
     rootProject.version = (if (ver.first().equals('v', true)) ver.substring(1) else ver.uppercase()).uppercase()
+}
+
+mavenPublishing {
+    coordinates(
+        groupId = "io.github.milkdrinkers",
+        artifactId = "maquillage",
+        version = rootProject.version.toString().let { originalVersion ->
+            if (!originalVersion.contains("-SNAPSHOT"))
+                originalVersion
+            else
+                originalVersion.substringBeforeLast("-SNAPSHOT") + "-SNAPSHOT" // Force append just -SNAPSHOT if snapshot version
+        }
+    )
+
+    pom {
+        name.set(rootProject.name)
+        description.set(rootProject.description.orEmpty())
+        url.set("https://github.com/milkdrinkers/Maquillage")
+        inceptionYear.set("2025")
+
+        licenses {
+            license {
+                name.set("GNU General Public License Version 3")
+                url.set("https://www.gnu.org/licenses/gpl-3.0.en.html#license-text")
+                distribution.set("https://www.gnu.org/licenses/gpl-3.0.en.html#license-text")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("rooooose-b")
+                name.set("Rose")
+                url.set("https://github.com/rooooose-b")
+                organization.set("Milkdrinkers")
+            }
+            developer {
+                id.set("darksaid98")
+                name.set("darksaid98")
+                url.set("https://github.com/darksaid98")
+                organization.set("Milkdrinkers")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/milkdrinkers/Maquillage")
+            connection.set("scm:git:git://github.com/milkdrinkers/Maquillage.git")
+            developerConnection.set("scm:git:ssh://github.com:milkdrinkers/Maquillage.git")
+        }
+    }
+
+    configure(JavaLibrary(
+        javadocJar = JavadocJar.None(), // We want to use our own javadoc jar
+    ))
+
+    // Publish to Maven Central
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
+
+    // Sign all publications
+    signAllPublications()
+
+    // Skip signing for local tasks
+    tasks.withType<Sign>().configureEach { onlyIf { !gradle.taskGraph.allTasks.any { it is PublishToMavenLocal } } }
 }
